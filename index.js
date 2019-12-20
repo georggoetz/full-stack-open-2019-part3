e742a7b962856fcba4ac0783500a0d6e2d6282db
georggoetz <georggoetz2@googlemail.com>
2019-12-20 16:25:44 +0100

    Finished 3.8

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 
-const persons = [
+morgan.token('body', req => {
+  if (req.body) {
+    return JSON.stringify(req.body)
+  }
+})
+
+app.use(bodyParser.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
   {
     "name": "Arto Hellas",
     "number": "040-123456",
@@ -24,8 +35,57 @@ const persons = [
   }
 ]
 
+const generateId = () => {
+  const maxId = Math.max(...persons.map(p => p.id))
+  return maxId + 1
+}
+
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({error: 'name missing'})
+  }
+  if (!body.number) {
+    return res.status(400).json({error: 'number missing'})
+  }
+  if (persons.find(p => p.name === body.name)) {
+    return res.status(400).json({error: 'name must be unique'})
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(newPerson)
+
+  res.json(newPerson)
+})
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date().toString()}</p>`)
 })
 
 const PORT = 3001
